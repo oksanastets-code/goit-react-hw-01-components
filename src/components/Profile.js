5abc82f34b59bd8ddfd7df6446620868f9a0bdb6
Oksana Stets <oksanastets73@gmail.com>
2021-12-08 14:53:25 +0200

    Update Profile.js
    add PropTypes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 export default function Profile({ avatar, username, tag, location, followers, views, likes }) {
   return (
     <div class="profile">
@@ -25,3 +27,12 @@ export default function Profile({ avatar, username, tag, location, followers, vi
     </div>
   );
 }
+Profile.propTypes = {
+  avatar: PropTypes.string,
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  followers: PropTypes.number,
+  views: PropTypes.number,
+  likes: PropTypes.number,
+};
