406a6d82eb9a638d15dffc9b7982cf8e17aa6db9
Oksana Stets <oksanastets73@gmail.com>
2021-12-14 15:29:38 +0200

    Update Profile.js

--- src/components/social-profile/Profile.js
+++ src/components/social-profile/Profile.js
@@ -47,32 +47,34 @@ export default function Profile({
       </Stats>
     </ProfileDiv>
   );
-  // return (
-  //   <div className={s.profile}>
-  //     <div className={s.description}>
-  //       <img src={avatar} alt="User avatar" width="150" className={s.avatar} />
-  //       <p className={s.name}>{username}</p>
-  //       <p className={s.tag}>@{tag}</p>
-  //       <p className={s.location}>{location}</p>
-  //     </div>
-
-  //     <ul className={s.stats}>
-  //       <li className={s.item}>
-  //         <span className={s.label}>Followers</span>
-  //         <span className={s.quantity}>{followers}</span>
-  //       </li>
-  //       <li className={s.item}>
-  //         <span className={s.label}>Views</span>
-  //         <span className={s.quantity}>{views}</span>
-  //       </li>
-  //       <li className={s.item}>
-  //         <span className={s.label}>Likes</span>
-  //         <span className={s.quantity}>{likes}</span>
-  //       </li>
-  //     </ul>
-  //   </div>
-  // );
 }
+// Option with css-module
+// return (
+//   <div className={s.profile}>
+//     <div className={s.description}>
+//       <img src={avatar} alt="User avatar" width="150" className={s.avatar} />
+//       <p className={s.name}>{username}</p>
+//       <p className={s.tag}>@{tag}</p>
+//       <p className={s.location}>{location}</p>
+//     </div>
+
+//     <ul className={s.stats}>
+//       <li className={s.item}>
+//         <span className={s.label}>Followers</span>
+//         <span className={s.quantity}>{followers}</span>
+//       </li>
+//       <li className={s.item}>
+//         <span className={s.label}>Views</span>
+//         <span className={s.quantity}>{views}</span>
+//       </li>
+//       <li className={s.item}>
+//         <span className={s.label}>Likes</span>
+//         <span className={s.quantity}>{likes}</span>
+//       </li>
+//     </ul>
+//   </div>
+// );
+
 Profile.propTypes = {
   avatar: PropTypes.string,
   username: PropTypes.string,
